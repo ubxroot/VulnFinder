2c056a6d0ab7d8547eece6fb0e62b95fd8bfc5fc
ubxroot <umangbhatt665@gmail.com>
2025-07-08 19:25:32 +0530

    Update vulnfinder.py

--- vulnfinder.py
+++ vulnfinder.py
@@ -3,7 +3,7 @@ VulnFinder - Main Entry Point
 
 This script launches the VulnFinder CLI application.
 """
-from vulnfinder.cli import app
+from cli import app
 
 if __name__ == "__main__":
     app() 
